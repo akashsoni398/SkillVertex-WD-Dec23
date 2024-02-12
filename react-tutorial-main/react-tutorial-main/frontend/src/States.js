026f07432e90b5c493bb38f59bebc330b0638eed
akashsoni398 <84252471+akashsoni398@users.noreply.github.com>
2024-02-12 21:55:55 +0530

    commit-react-2

--- react-tutorial-main/react-tutorial-main/frontend/src/States.js
+++ react-tutorial-main/react-tutorial-main/frontend/src/States.js
@@ -4,22 +4,44 @@ class States extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: "John Doe"
+            name: "John Doe",
+            time: ""
         }
         this.handleClick = this.handleClick.bind(this);
+        this.setTime = this.setTime.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
-    handleClick() {
+    handleClick(event) {
+        console.log(event);
         let newName = document.getElementById('name-input').value;
         this.setState( {
             name: newName
         })
     }
+
+    handleKeyPress(event) {
+        console.log(event.key);
+    }
+
+    setTime() {
+        let timeFn = new Date();
+        let hours = timeFn.getHours();
+        let minutes = timeFn.getMinutes();
+        let seconds = timeFn.getSeconds();
+        let time = hours+':'+minutes+':'+seconds;
+        this.setState({time: time})
+    }
+
+    componentDidMount() {
+        setInterval(()=>this.setTime(),1000)
+    }
     render() {
         return (
             <div>
-                <h3>Hi! Im {this.state.name}</h3>
-                <input type="text" id="name-input" />
-                <button onClick={this.handleClick}>Update Name</button>;
+                <h3>Hi! Im {this.state.name}.</h3>
+                <input type="text" id="name-input" onKeyPress={this.handleKeyPress} />
+                <button onClick={this.handleClick}>Update Name</button>
+                <div>{this.state.time}</div>
             </div>
         )
     }
--- react-tutorial-main/react-tutorial-main/frontend/src/index.js
+++ react-tutorial-main/react-tutorial-main/frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import States from './States';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<States />);
 
